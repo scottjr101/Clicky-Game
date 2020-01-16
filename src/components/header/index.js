feb1192fc1966aba905a68ab38b3ad64077c9b8a
Scott Ladd <scottjr101@gmail.com>
2020-01-16 14:36:37 -0500

    added header section

---
+++ src/components/header/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './styles.css';
+
+function Header() {
+  return <div className="headerCustom container-fluid text-center"><h1>Do you love me?</h1></div>;
+}
+
+export default Header;
