4d56d0febe69413ddebf86891507e7974f366399
Scott Ladd <scottjr101@gmail.com>
2020-01-16 14:36:18 -0500

    added about section

---
+++ src/components/About/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+function About() {
+  return <div className="aboutCustom container-fluid text-center"><h1>Can you learn to love me?</h1></div>;
+}
+
+export default About;
