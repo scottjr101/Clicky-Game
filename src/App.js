b3145384e2fbd16e3736d208d5d1c1a751b89203
Scott Ladd <scottjr101@gmail.com>
2020-01-16 14:38:33 -0500

    added componets in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import './App.css';
+import Container from './components/container';
+import Row from './components/row';
+import Header from './components/header';
+import About from './components/About';
+import Cards from './components/greggCards';
+import Gregs from './gregs.json';
 
 
 
 class App extends Component {
+state = { Gregs };
 
 
   render() {
     return (
       <>
-      <h1>Hello</h1>
+      <Container>
+      <Row>
+        <Header>
+        </Header>
+      </Row>
+      <Row>
+        <About>
+        </About>
+      </Row>
+      <Row>
+        <div className="container-fluid d-flex justify-content-center align-content-start flex-wrap">
+      {this.state.Gregs.map(friend => (
+          <Cards
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+          />
+        ))}
+        </div>
+      </Row>
+      </Container>
       </>
     );
   }
