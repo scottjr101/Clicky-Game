77389379ca5652fcf01a39fa49937d96c576a8ab
Scott Ladd <scottjr101@gmail.com>
2020-01-17 17:09:28 -0500

    added scores and message

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import './styles.css';
 
-function Header() {
-  return <div className="headerCustom sticky-top container-fluid text-center"><h1>Do you love me?</h1></div>;
+function Header(props) {
+  return <div className="headerCustom sticky-top container-fluid d-flex justify-content-between align-content-center flex-wrap">
+           <h3>{props.clickedMessage}</h3>
+           <h3>Current Score: {props.correctGuess}</h3>
+           <h3>High Score: {props.highScore}</h3>
+         </div>;
 }
 
 export default Header;
