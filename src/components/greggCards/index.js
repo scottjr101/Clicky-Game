b902222fec6aaaea8d291cf5590ef549ee4eeb7d
Scott Ladd <scottjr101@gmail.com>
2020-01-17 17:10:28 -0500

    added on click

--- src/components/greggCards/index.js
+++ src/components/greggCards/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 
 function Cards(props) {
-    return <div className="card border-0">
+    return <div className="card border-0" onClick={() => props.clicker(props.id)}>
               <img src={props.image} className="card-img-top" alt={props.name} />
             </div>
             
