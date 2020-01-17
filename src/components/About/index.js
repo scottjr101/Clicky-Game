1c59c2796c164d7d41ff348d519365700d0daf0d
Scott Ladd <scottjr101@gmail.com>
2020-01-17 00:35:50 -0500

    added flex and text to sections

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import './styles.css';
 
 function About() {
-  return <div className="aboutCustom container-fluid text-center"><h1>Can you learn to love me?</h1></div>;
+  return <div className="aboutCustom container-fluid d-flex justify-content-center align-content-center flex-wrap">
+          <div className="text-center">
+          <h3>Listen here motha licker! You pulled me up with your big strong arms. Now you have to do something for old Gregg.</h3>
+          <h3>Click on them images down there, but don't hit the same one twice. If you do I'll show you my downstairs mix up.</h3>
+          </div>
+         </div>;
 }
 
 export default About;
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './styles.css';
 
 function Header() {
-  return <div className="headerCustom container-fluid text-center"><h1>Do you love me?</h1></div>;
+  return <div className="headerCustom sticky-top container-fluid text-center"><h1>Do you love me?</h1></div>;
 }
 
 export default Header;
