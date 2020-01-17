ea912208a512014627faf6c1c017480467ded8f1
Scott Ladd <scottjr101@gmail.com>
2020-01-17 17:08:34 -0500

    added github and portfolio buttons

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './styles.css';
 
 function Footer() {
-    return <div className="footerCustom container-fluid text-center"></div>
+    return <div className="footerCustom container-fluid d-flex justify-content-around align-content-center flex-wrap">  
+              <a href="https://github.com/scottjr101/Clicky-Game">
+                <button type="button" className="btn btn-dark">GitHub</button>
+              </a>
+              <a href="https://scottjr101.github.io/">
+                <button type="button" className="btn btn-dark">Portfolio</button>
+              </a>
+            </div>
 }
 
 export default Footer;
